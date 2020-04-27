dc54a770a80b1fc948df152cb029ab338161de63
John Guerrero <guerrerojohnalexander@gmail.com>
2020-04-26 21:46:33 -0400

    Se agrego validacion de autenticacion, para sacar al usuario al login si no ha iniciado sesion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\File;
 
 class UserController extends Controller{
 
+    public function __construct(){
+
+        $this->middleware('auth');
+
+    }
 
     // Cargar vista de configuración
     public function config()    {
