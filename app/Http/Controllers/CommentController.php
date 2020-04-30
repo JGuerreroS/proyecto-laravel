e7b4638e632b1c58d916165c0142e5a350944285
John Guerrero <guerrerojohnalexander@gmail.com>
2020-04-30 17:30:42 -0400

    Se agrego funcionalidad de eliminar comentarios

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -40,10 +40,32 @@ class CommentController extends Controller{
             'message' => 'Comentario públicado correctamente'
         ]);
 
-        var_dump($content);
-        var_dump($image_id);
-        die();
+    }
 
+    public function delete($id){
+        
+        // Conseguir datos del usuario identificado
+        $user = \Auth::user();
+
+        // Conseguir objeto del comentario
+        $comment = Comment::find($id);
+
+        // Comprobar si soy el dueño del comentario o publicación
+        if($user && ($comment->user_id == $user->id || $comment->image->user_id == $user->id)){
+
+            $comment->delete(); // Se elimina el comentario
+
+            // Redirección
+            return redirect()->route('image.detail', ['id' => $comment->image->user_id])->with([
+                'message' => 'Comentario eliminado correctamente'
+            ]);
+
+        }else{
+            // Redirección
+            return redirect()->route('image.detail', ['id' => $comment->image->user_id])->with([
+                'message' => 'El comentario no se ha eliminado'
+            ]);
+        }
     }
 
 }
--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -80,9 +80,23 @@
 
                             @foreach ($image->comments as $comment)
                                 <div class="comment">
+
                                     <span class="nickname"> {{ '@'.$comment->user->nick }} </span>
+
                                     <span class="nickname"> {{ ' | '.\FormatTime::LongTimeFilter($comment->created_at) }} </span>
-                                    <p> {{ $comment->content }} </p>
+
+                                    <p> {{ $comment->content }} 
+
+                                        <br>
+
+                                        @if(Auth::check() && ($comment->user_id == Auth::user()->id || $comment->image->user_id == Auth::user()->id))    
+                                            <a href="{{ route('comment.delete', ['id' => $comment->id]) }}" class="btn btn-sm btn-danger">
+                                                Eliminar
+                                            </a>
+                                        @endif
+                                        
+                                    </p>
+
                                 </div>
                             @endforeach
 
