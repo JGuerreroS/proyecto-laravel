1b9f4f6a28e0e913005ad31fcbcdd374ab17f5b8
John Guerrero <guerrerojohnalexander@gmail.com>
2020-04-30 15:06:35 -0400

    Guardar comentarios de la publicaciones en la base de datos

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
 
 class CommentController extends Controller{
 
@@ -19,9 +20,25 @@ class CommentController extends Controller{
             'image_id' => 'integer|required',
             'content' => 'string|required'
         ]);
-        
+
+        // Recoger datos del formulario
+        $user = \Auth::user()->id;
         $image_id = $request->input('image_id');
         $content = $request->input('content');
+
+        // Asignar valores al objeto
+        $comment = new Comment();
+        $comment->user_id = $user;
+        $comment->image_id = $image_id;
+        $comment->content = $content;
+
+        // Guardar en la base de datos
+        $comment->save();
+
+        // Redirección
+        return redirect()->route('image.detail', ['id' => $image_id])->with([
+            'message' => 'Comentario públicado correctamente'
+        ]);
 
         var_dump($content);
         var_dump($image_id);
