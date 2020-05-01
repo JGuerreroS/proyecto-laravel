0ecb9c037229870e37bbda7efc6abcc7ba5f238b
John Guerrero <guerrerojohnalexander@gmail.com>
2020-05-01 15:42:52 -0400

    Se agregaron las funciones Javascript de like y dislike

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
 window.addEventListener("load", function(){
-    alert('msg');
+
+    like();
+    dislike();
+
+    $('.btn-like').css('cursor','pointer');
+    $('.btn-dislike').css('cursor','pointer');
+
+    // Boton like
+    function like(){
+        $('.btn-like').unbind('click').click(function(){
+            console.log('Like');
+            $(this).addClass('btn-dislike').removeClass('btn-like');
+            $(this).attr('src','img/heart-red.png');
+            dislike();
+        });
+    }
+
+    // Boton dislike
+    function dislike(){
+        $('.btn-dislike').unbind('click').click(function(){
+            console.log('Dislike');
+            $(this).addClass('btn-like').removeClass('btn-dislike');
+            $(this).attr('src','img/heart-gray.png');
+            like();
+        });
+    }
+    
 });
