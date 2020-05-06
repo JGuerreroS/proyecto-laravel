593f9b63e7071f5dd751445d3f2ade250a67cc7c
John Guerrero <guerrerojohnalexander@gmail.com>
2020-05-06 13:19:33 -0400

    Agregar modal antes de eliminar la publicacion

--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -73,7 +73,32 @@
                         @if (Auth::user() && Auth::user()->id == $image->user->id)    
                             <div class="actions">
                                 <a href="" class="btn btn-sm btn-primary">Actualizar</a>
-                                <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn btn-sm btn-danger">Eliminar</a>
+                                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                    Eliminar
+                                </button>
+
+                                {{-- Modal de eliminar publicación --}}
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">¿Estas seguro?</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                        </div>
+                                        <div class="modal-body">
+                                        Si eliminas esta imagen no podras volver a recuperarla
+                                        </div>
+                                        <div class="modal-footer">
+                                        <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
+                                        <a href="{{ route('image.delete', ['id' => $image->id]) }}" class="btn  btn-danger">Eliminar</a>
+
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
                             </div>
                         @endif
 
